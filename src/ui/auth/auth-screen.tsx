07ffc1429624559f35ae664b772e192e5583541c
gogo <go.o6.22.22@gmail.com>
2022-09-19 01:28:59 +0900

    auth screen wip

--- src/ui/auth/auth-screen.tsx
+++ src/ui/auth/auth-screen.tsx
@@ -1,26 +1,39 @@
 import React, { useState } from 'react'
-import { SafeAreaView, View } from 'react-native'
+import { SafeAreaView, TouchableWithoutFeedback, View } from 'react-native'
 import styled from 'styled-components'
 import { DescBody2, H1 } from 'ui/common/text'
 import { Input } from 'ui/common/input'
+import { keyboardStore } from 'store/keyboard'
 
 export const AuthScreen = () => {
   const [phone, setPhone] = useState('')
+  const [code, setCode] = useState('')
   return (
-    <SafeAreaView>
-      <Container>
-        <TextArea>
-          <H1>반가워요 :)</H1>
-          <H1 style={{ marginBottom: 12 }}>휴대폰 번호를 입력해주세요!</H1>
-          <DescBody2>등록된 회원인지 확인하고 가입을 진행할게요 :)</DescBody2>
-        </TextArea>
-        <Input
-          placeholder='- 없이 숫자만 입력해주세요'
-          value={phone}
-          onChange={(v) => setPhone(v)}
-        ></Input>
-      </Container>
-    </SafeAreaView>
+    <TouchableWithoutFeedback onPress={() => keyboardStore.hide()}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Container>
+          <TextArea>
+            <H1>반가워요 :)</H1>
+            <H1 style={{ marginBottom: 12 }}>휴대폰 번호를 입력해주세요!</H1>
+            <DescBody2>등록된 회원인지 확인하고 가입을 진행할게요 :)</DescBody2>
+          </TextArea>
+          {false && (
+            <Input
+              placeholder='인증번호 6자리 입력'
+              value={code}
+              onChange={(v) => setCode(v)}
+              isError={false}
+            />
+          )}
+          <Input
+            placeholder='- 없이 숫자만 입력해주세요'
+            value={phone}
+            onChange={(v) => setPhone(v)}
+            isError={false}
+          />
+        </Container>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   )
 }
 
--- src/ui/common/input.tsx
+++ src/ui/common/input.tsx
@@ -1,5 +1,9 @@
-import React, { useEffect } from 'react'
-import { TextInput as _TextInput, View } from 'react-native'
+import React, { useRef, useState } from 'react'
+import {
+  TextInput as _TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native'
 import styled from 'styled-components'
 import { Colors } from 'infra/colors'
 
@@ -7,27 +11,39 @@ export const Input: React.FC<{
   value: string
   onChange: (v: string) => void
   placeholder: string
-}> = ({ value, onChange, placeholder }) => {
+  isError: boolean
+}> = ({ value, onChange, placeholder, isError }) => {
+  const inputRef = useRef<null | _TextInput>(null)
+  const [isFocused, setIsFocused] = useState(false)
   return (
-    <Container>
-      <TextInput
-        autoComplete='tel-device'
-        keyboardType='phone-pad'
-        textContentType='telephoneNumber'
-        placeholder={placeholder}
-        placeholderTextColor={Colors.gray.v300}
-        onChangeText={(v) => onChange(v)}
-      >
-        {value}
-      </TextInput>
-    </Container>
+    <TouchableWithoutFeedback onPress={() => inputRef.current?.focus()}>
+      <Container isFocused={isFocused} isError={isError}>
+        <TextInput
+          ref={inputRef}
+          autoComplete='tel-device'
+          keyboardType='phone-pad'
+          textContentType='telephoneNumber'
+          placeholder={placeholder}
+          placeholderTextColor={Colors.gray.v500}
+          onChangeText={(v) => onChange(v)}
+          value={value}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+        />
+      </Container>
+    </TouchableWithoutFeedback>
   )
 }
 
-const Container = styled(View)`
+const Container = styled(View)<{
+  isFocused: boolean
+  isError: boolean
+}>`
   padding: 28px 20px;
-  border: 1px solid ${Colors.primary.blue};
+  border: 2px solid
+    ${(p) => (p.isFocused ? Colors.primary.blue : Colors.gray.v100)};
   border-radius: 16px;
+  background-color: ${(p) => (p.isFocused ? Colors.white : Colors.gray.v100)};
 `
 
 const TextInput = styled(_TextInput)`
