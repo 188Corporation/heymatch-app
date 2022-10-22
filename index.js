cce8dca1ec93618f8e8d01b9cc9871cea6b3a055
gogo <go.o6.22.22@gmail.com>
2022-10-22 12:05:30 +0900

    ignore navigation animation no listener warning

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import { AppRegistry } from 'react-native'
+import { AppRegistry, LogBox } from 'react-native'
 import { App } from 'app'
 
+// NOTE(gogo): may be removed after the bug is fixed
+// https://github.com/react-navigation/react-navigation/issues/7839#issuecomment-1281439945
+LogBox.ignoreLogs([
+  'Sending `onAnimatedValueUpdate` with no listeners registered.',
+])
 AppRegistry.registerComponent('HeyThereApp', () => App)
