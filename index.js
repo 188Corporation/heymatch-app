86f421990ee90a0981827b5cb6ae054cf9aafab7
gogo <go.o6.22.22@gmail.com>
2022-10-31 19:02:51 +0900

    add version & codepush script

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import { AppRegistry, LogBox } from 'react-native'
 import { App } from 'app'
+import codePush from 'react-native-code-push'
+import { IS_DEV } from 'infra/constants'
 
 // NOTE(gogo): may be removed after the bug is fixed
 // https://github.com/react-navigation/react-navigation/issues/7839#issuecomment-1281439945
 LogBox.ignoreLogs([
   'Sending `onAnimatedValueUpdate` with no listeners registered.',
 ])
-AppRegistry.registerComponent('HeyThereApp', () => App)
+AppRegistry.registerComponent('HeyThereApp', () =>
+  IS_DEV
+    ? App
+    : codePush({
+        checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+        installMode: codePush.InstallMode.ON_NEXT_RESTART,
+        mandatoryInstallMode: codePush.InstallMode.IMMEDIATE,
+      })(App),
+)
