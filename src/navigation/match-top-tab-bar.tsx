5002f903af6ffa6b66eeac4f372bb6ad546d4de7
gogo <go.o6.22.22@gmail.com>
2022-11-14 00:17:40 +0900

    top tab bar focus indicator animation successful

--- src/navigation/match-top-tab-bar.tsx
+++ src/navigation/match-top-tab-bar.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { MaterialTopTabBarProps } from '@react-navigation/material-top-tabs'
-import { Animated, TouchableOpacity, View } from 'react-native'
+import { Animated, TouchableOpacity } from 'react-native'
 import { genTabBarCommon } from 'navigation/common'
+import styled from 'styled-components'
+import { Row } from 'ui/common/layout'
+import { WINDOW_DIMENSIONS } from 'infra/constants'
+
+const HORIZONTAL_PADDING = 10
+const FOCUS_INDICATOR_WIDTH =
+  (WINDOW_DIMENSIONS.width - HORIZONTAL_PADDING * 2) / 2
 
 export const MatchTopTabBar: React.FC<MaterialTopTabBarProps> = ({
   state,
@@ -9,8 +16,15 @@ export const MatchTopTabBar: React.FC<MaterialTopTabBarProps> = ({
   navigation,
   position,
 }) => {
+  const inputRange = state.routes.map((_, i) => i)
+  const translateX = position.interpolate({
+    inputRange,
+    outputRange: inputRange.map(
+      (i) => HORIZONTAL_PADDING + i * FOCUS_INDICATOR_WIDTH,
+    ),
+  })
   return (
-    <View style={{ flexDirection: 'row', backgroundColor: 'black' }}>
+    <BarContainer>
       {state.routes.map((route, index) => {
         const { options } = descriptors[route.key]
         const label = options.tabBarLabel as string
@@ -20,13 +34,8 @@ export const MatchTopTabBar: React.FC<MaterialTopTabBarProps> = ({
           navigation,
           isFocused,
         )
-        const inputRange = state.routes.map((_, i) => i)
-        const opacity = position.interpolate({
-          inputRange,
-          outputRange: inputRange.map((i) => (i === index ? 1 : 0)),
-        })
         return (
-          <TouchableOpacity
+          <BarButton
             key={index}
             accessibilityRole='button'
             accessibilityState={isFocused ? { selected: true } : {}}
@@ -36,10 +45,33 @@ export const MatchTopTabBar: React.FC<MaterialTopTabBarProps> = ({
             onLongPress={onLongPress}
             style={{ flex: 1 }}
           >
-            <Animated.Text style={{ opacity }}>{label}</Animated.Text>
-          </TouchableOpacity>
+            <Animated.Text style={{ color: isFocused ? 'black' : 'green' }}>
+              {label}
+            </Animated.Text>
+          </BarButton>
         )
       })}
-    </View>
+      <FocusIndicator style={{ transform: [{ translateX }] }} />
+    </BarContainer>
   )
 }
+
+const BarContainer = styled(Row)`
+  background-color: red;
+  position: relative;
+`
+
+const BarButton = styled(TouchableOpacity)`
+  align-items: center;
+  justify-content: center;
+  padding: 16px 0;
+`
+
+const FocusIndicator = styled(Animated.View)`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: ${FOCUS_INDICATOR_WIDTH}px;
+  height: 10px;
+  background-color: black;
+`
