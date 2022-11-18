37916de9847169b7466f24d8fbdb59b6dc0ab5a7
gogo <go.o6.22.22@gmail.com>
2022-11-19 02:10:03 +0900

    purchase history screen done

--- src/infra/datetime.ts
+++ src/infra/datetime.ts
@@ -6,6 +6,9 @@ dayjs.extend(customParseFormat)
 
 export const formatDate = (d: ConfigType) => dayjs(d).format('YYYY년 M월 D일')
 
+export const formatDateTime = (d: ConfigType) =>
+  dayjs(d).format('M월 D일 HH:mm')
+
 export const parseDateHeaderDateString = (s: string) => {
   return dayjs(s.trim(), 'M월 D')
 }
--- src/ui/my/purchase-history-screen.tsx
+++ src/ui/my/purchase-history-screen.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 import { NavigationHeader } from 'ui/common/navigation-header'
 import { ScrollView } from 'react-native'
 import { useMy } from 'api/reads'
+import { UserPurchase } from 'infra/types'
+import styled from 'styled-components'
+import { Column, Row } from 'ui/common/layout'
+import { Body, Caption, H3 } from 'ui/common/text'
+import { Image } from 'ui/common/image'
+import { CandyIconPng } from 'image'
+import { formatPrice } from 'infra/util'
+import { formatDateTime } from 'infra/datetime'
 
 export const PurchaseHistoryScreen = () => {
   const { data } = useMy()
@@ -9,7 +17,37 @@ export const PurchaseHistoryScreen = () => {
   return (
     <>
       <NavigationHeader backButtonStyle='black' title='결제내역' />
-      <ScrollView></ScrollView>
+      <ScrollView>
+        {data.user_purchases.map((x) => (
+          <Item key={x.id} data={x} />
+        ))}
+      </ScrollView>
     </>
   )
 }
+
+const Item: React.FC<{
+  data: UserPurchase
+}> = ({ data }) => {
+  return (
+    <ItemContainer>
+      <Caption style={{ marginBottom: 16 }}>
+        {formatDateTime(data.purchased_at)}
+      </Caption>
+      <Row style={{ justifyContent: 'space-between' }}>
+        <Row style={{ alignItems: 'center' }}>
+          <Image
+            source={CandyIconPng}
+            style={{ width: 24, height: 24, marginRight: 12 }}
+          />
+          <Body>{data.point_item.name}</Body>
+        </Row>
+        <H3>{formatPrice(data.point_item.price_in_krw)}</H3>
+      </Row>
+    </ItemContainer>
+  )
+}
+
+const ItemContainer = styled(Column)`
+  padding: 16px 24px;
+`
