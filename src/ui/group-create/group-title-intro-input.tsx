16002d8cdf384fb9da7d53c454579583c78def3f
gogo <go.o6.22.22@gmail.com>
2022-12-04 20:44:59 +0900

    fix placeholder text

--- src/ui/group-create/group-title-intro-input.tsx
+++ src/ui/group-create/group-title-intro-input.tsx
@@ -28,7 +28,7 @@ export const GroupTitleIntroInput = observer(() => {
             style={{ flex: 1, height: 200, lineHeight: 28 }}
             value={groupCreateStore.intro}
             onChangeText={(v) => groupCreateStore.setIntro(v)}
-            placeholder='ex) 오랜만에 셋이서 이태원 놀러왔어요 :) 간맥하는 중인데 같이 파티할 사람친구 구해요! (10자 이상)'
+            placeholder='ex) 오랜만에 셋이서 동성로 놀러왔어요 :) 간맥하는 중인데 같이 파티할 사람친구 구해요! (10자 이상)'
             multiline
             textAlignVertical='top'
           />
