e05c26c8dab167a6ee0fe05ba5f21366ee2f267d
gogo <go.o6.22.22@gmail.com>
2022-12-05 14:33:48 +0900

    release 1.0.3, build 20

--- src/infra/version.ts
+++ src/infra/version.ts
@@ -1,5 +1,5 @@
-export const CODEPUSH_VERSION = '1.0.2.0'
+export const CODEPUSH_VERSION = '1.0.3.0'
 // WARNING(gogo): [네이티브 패키지 + JS] 변경사항 있을 때는 반드시 코드푸시 버전 분리해야 함!
 // 네이티브 버전 올릴 때는 기존 main 브랜치를 새로운 브랜치로 분기하자(브랜치 이름은 기존 버전 코드로)
-// yarn ver 1.0.2
-// yarn cp-a -m -t '1.0.2' && yarn cp-i -m -t '1.0.2'
+// yarn ver 1.0.3
+// yarn cp-a -m -t '1.0.3' && yarn cp-i -m -t '1.0.3'
