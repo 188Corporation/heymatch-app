f84b4b4e839fbf52bba2ca41ba3c65f60f0de37e
hjjo <compa513@naver.com>
2023-03-05 17:24:15 +0900

    setup reactotron-react-native
    to debug api call, async-storage

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { RootStacks } from 'navigation/root-stacks'
+import { ChatProvider } from 'infra/chat'
 import { Colors } from 'infra/colors'
+import { CURRENT_OS, OS } from 'infra/constants'
+import { toastConfig } from 'infra/toast-config'
+import { _navigationRef } from 'navigation/global'
+import { RootStacks } from 'navigation/root-stacks'
+import React from 'react'
+import { StatusBar } from 'react-native'
+import Toast from 'react-native-toast-message'
 import { StoresProvider } from 'store/globals'
 import { AlertModal } from 'ui/common/alert-modal'
-import { _navigationRef } from 'navigation/global'
-import { ChatProvider } from 'infra/chat'
-import { StatusBar } from 'react-native'
-import { CURRENT_OS, OS } from 'infra/constants'
-import Toast from 'react-native-toast-message'
-import { toastConfig } from 'infra/toast-config'
+
+if (__DEV__) {
+  import('../ReactotronConfig').then(() => console.log('Reactotron Configured'))
+}
 
 export const App = () => {
   // NOTE(gogo): all init code should go under root stack
