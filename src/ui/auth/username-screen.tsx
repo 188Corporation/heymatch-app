4df4b840c2d59db793c1f2d7b6a5e4ef9002d7d7
hjjo <compa513@naver.com>
2023-06-18 15:13:59 +0900

    닉네임 입력 규칙 적용(2글자이상 10글자 이하, 한글영어숫자만 입력가능, 자음모음만으로 이루어진 한글 불가

--- src/ui/auth/username-screen.tsx
+++ src/ui/auth/username-screen.tsx
@@ -16,11 +16,49 @@ import { KeyboardAvoidingView } from 'ui/common/keyboard-avoiding-view'
 import { NavigationHeader } from 'ui/common/navigation-header'
 import { Body2, Caption, DescBody2, H1 } from 'ui/common/text'
 
+type InvalidReason = 'INVALID_LENGTH' | 'INVALID_INPUT'
+
 export const UsernameScreen = observer(() => {
   const { data } = useOnboardingStatus()
   const { userProfileStore } = useStores()
   const [isUnique, setIsUnique] = useState<boolean | null>(null)
   const isEditing = data?.status === 'onboarding_completed'
+  const [invalidUsername, setInvalidUsername] = useState<InvalidReason | null>(
+    null,
+  )
+
+  const validateUsername = (username: string) => {
+    if (username.length < 2 || username.length > 10) {
+      setInvalidUsername('INVALID_LENGTH')
+      return false
+    }
+    const regex = /^[가-힣a-zA-Z0-9]+$/
+    if (regex.test(username)) {
+      if (/^[가-힣]+$/.test(username)) {
+        if (!/^[^가-힣]*[ㄱ-ㅎㅏ-ㅣ]*[^가-힣]*$/.test(username)) {
+          setInvalidUsername(null)
+          return true
+        } else {
+          setInvalidUsername('INVALID_INPUT')
+          return false
+        }
+      } else {
+        setInvalidUsername(null)
+        return true
+      }
+    } else {
+      setInvalidUsername('INVALID_INPUT')
+      return false
+    }
+  }
+
+  const getInvalidUsernameReason = () => {
+    if (invalidUsername === 'INVALID_LENGTH') {
+      return '닉네임은 2글자 이상으로 입력해주세요'
+    } else if (invalidUsername === 'INVALID_INPUT') {
+      return '한글, 영어, 숫자만 입력할 수 있어요. (자음, 모음만 있는 한글은 처리하지 않습니다.)'
+    }
+  }
 
   return (
     <KeyboardAvoidingView>
@@ -44,9 +82,15 @@ export const UsernameScreen = observer(() => {
               onValueChange={(v) => {
                 userProfileStore.setUsername(v)
                 setIsUnique(null)
+                setInvalidUsername(null)
               }}
               letterCase='lower'
             />
+            {invalidUsername && (
+              <Body2 style={{ marginTop: 4, color: Colors.primary.red }}>
+                {getInvalidUsernameReason()}
+              </Body2>
+            )}
             {isUnique === false && (
               <Body2 style={{ marginTop: 4, color: Colors.primary.red }}>
                 이미 존재하는 닉네임이에요!
@@ -59,6 +103,9 @@ export const UsernameScreen = observer(() => {
             )}
             <SubButton
               onPress={async () => {
+                if (!validateUsername(userProfileStore.username)) {
+                  return
+                }
                 try {
                   await checkUsername(userProfileStore.username)
                   await mutate('/users/my/')
