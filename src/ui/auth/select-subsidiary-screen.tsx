68221b700264f8898fdf90bff40234b474f54945
hjjo <compa513@naver.com>
2023-07-26 00:10:58 +0900

    계열사 선택화면 스크롤 적용

--- src/ui/auth/select-subsidiary-screen.tsx
+++ src/ui/auth/select-subsidiary-screen.tsx
@@ -1,30 +1,43 @@
 import { CheckSvg } from 'image'
 import { Colors } from 'infra/colors'
+import { BOTTOM_BUTTON_HEIGTH, NAVIGATION_HEADER_HEIGHT } from 'infra/constants'
+import { useSafeAreaInsets } from 'infra/util'
 import { navigation } from 'navigation/global'
 import React, { useState } from 'react'
-import { TouchableOpacity, View } from 'react-native'
+import { Dimensions, ScrollView, TouchableOpacity, View } from 'react-native'
 import { useStores } from 'store/globals'
 import styled from 'styled-components'
 import { BottomButton } from 'ui/common/bottom-button'
-import { FlexScrollView } from 'ui/common/flex-scroll-view'
 import { NavigationHeader } from 'ui/common/navigation-header'
 import { Body, DescBody2, H1 } from 'ui/common/text'
 
+const OTHER_SPACE_HEIHGT = 119
+
 export const SelectSubsidiaryScreen = () => {
   const { userProfileStore } = useStores()
+  const insets = useSafeAreaInsets()
   const [selectedName, setSelectedName] = useState<string | null>(null)
   return (
     <>
       <NavigationHeader backButtonStyle='black' title='' />
       <View style={{ flexGrow: 1 }}>
-        <FlexScrollView>
-          <Container>
-            <View style={{ marginBottom: 40 }}>
-              <H1 style={{ marginBottom: 12 }}>계열사를 선택해주세요</H1>
-              <DescBody2>
-                정보를 더 알려주시면 빠른 매칭에 도움이 돼요 :)
-              </DescBody2>
-            </View>
+        <Container>
+          <View style={{ marginBottom: 40 }}>
+            <H1 style={{ marginBottom: 12 }}>계열사를 선택해주세요</H1>
+            <DescBody2>
+              정보를 더 알려주시면 빠른 매칭에 도움이 돼요 :)
+            </DescBody2>
+          </View>
+          <ScrollView
+            style={{
+              height:
+                Dimensions.get('window').height -
+                (BOTTOM_BUTTON_HEIGTH +
+                  NAVIGATION_HEADER_HEIGHT +
+                  insets.top +
+                  OTHER_SPACE_HEIHGT),
+            }}
+          >
             {userProfileStore.verifiedOrganizationNames!.map(
               (subsidiaryName) => {
                 return (
@@ -58,8 +71,8 @@ export const SelectSubsidiaryScreen = () => {
                 )
               },
             )}
-          </Container>
-        </FlexScrollView>
+          </ScrollView>
+        </Container>
       </View>
       <BottomButton
         text='선택하기'
